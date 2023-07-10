9e9108649a62339553b52daf2d0dda781ad81940
Рома <pomo4444ka@gmail.com>
2023-07-10 17:38:14 +0300

    Старт

---
+++ src/main/java/com/course/demojavacore/DemojavacoreApplication.java
@@ -0,0 +1,13 @@
+package com.course.demojavacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojavacoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojavacoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/course/demojavacore/DemojavacoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.demojavacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojavacoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
